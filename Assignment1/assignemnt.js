df773a546747b5aa3792fbbdebf15d9f04460f98
Mrudula <mrudula.mm@coddletech.com>
2023-08-30 15:13:25 +0530

    second commit

--- Assignment1/assignemnt.js
+++ Assignment1/assignemnt.js
@@ -1,23 +1,9 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable prefer-template */
-/* eslint-disable linebreak-style */
 /* eslint-disable no-console */
-/* eslint-disable linebreak-style */
-/* eslint-disable no-unused-vars */
-/* eslint-disable linebreak-style */
-/* eslint-disable eol-last */
-/* eslint-disable linebreak-style */
-// eslint-disable-next-line linebreak-style
-// eslint-disable-next-line no-console, linebreak-style
-// eslint-disable-next-line no-unused-vars
 const numberVariable = 25;
-// eslint-disable-next-line no-console
 console.log('numberVariable:', typeof numberVariable);
 const stringVariable = 'Ram';
-// eslint-disable-next-line no-console
 console.log('stringVariable:', typeof stringVariable);
 const booleanVariable = true;
-// eslint-disable-next-line no-console
 console.log('booleanVariable:', typeof booleanVariable);
 const nullVariable = null;
 console.log('nullVariable:', typeof nullVariable);
@@ -30,5 +16,5 @@ console.log('arrayVariable:', typeof arrayVariable);
 const usingTemplateLiterals = `${numberVariable} ${stringVariable} ${booleanVariable} ${nullVariable} ${undefinedVariable}`;
 console.log('usingTemplateLiterals:', usingTemplateLiterals);
 // eslint-disable-next-line no-useless-concat
-const withoutTempalteLiterals = numberVariable + ' ' + ' ' + stringVariable + ' ' + booleanVariable + ' ' + nullVariable + ' ' + undefinedVariable;
+const withoutTempalteLiterals = `${numberVariable} ` + ` ${stringVariable} ${booleanVariable} ${nullVariable} ${undefinedVariable}`;
 console.log('withoutTempalteLiterals:', withoutTempalteLiterals);

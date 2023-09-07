f4072833e196c3e504a4debf06ca59a593031003
Mrudula <mrudula.mm@coddletech.com>
2023-09-07 16:53:28 +0530

    commit

---
+++ Assignment6/js_troubleshooting (1)/js_troubleshooting/js/slider.js
@@ -0,0 +1,17 @@
+const a = ['1.jpg', '2.jpg', '3.jpg'];
+let selectedIndex = 0;
+
+function updateImg(type) {
+  if (type === '0') {
+    selectedIndex -= 1;
+  } else {
+    selectedIndex += 1;
+  }
+  if (selectedIndex < '0') {
+    selectedIndex = a.length - 1;
+  } else if (selectedIndex >= a.length) {
+    selectedIndex = 0;
+  }
+  // eslint-disable-next-line no-debugger
+  document.getElementById('imgs').src = `./images/${a[selectedIndex]}`;
+}
